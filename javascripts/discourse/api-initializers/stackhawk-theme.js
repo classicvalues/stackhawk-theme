30222f95250ee1479316bd9eb15d0d9d13148861
Keegan George <kgeorge13@gmail.com>
2022-03-01 12:46:35 -0800

    UX: Add custom icons

--- javascripts/discourse/api-initializers/stackhawk-theme.js
+++ javascripts/discourse/api-initializers/stackhawk-theme.js
@@ -1,3 +1,24 @@
 import { apiInitializer } from 'discourse/lib/api';
 
-export default apiInitializer('0.11.1', (api) => {});
+export default apiInitializer('0.11.1', (api) => {
+  // Replace Icons:
+  api.replaceIcon('bars', 'sh-bars');
+  api.replaceIcon('bell', 'sh-bell');
+  api.replaceIcon('caret-down', 'sh-caret-down');
+  api.replaceIcon('check-square', 'sh-check-square');
+  api.replaceIcon('check', 'sh-check');
+  api.replaceIcon('chevron-down', 'sh-chevron-down');
+  api.replaceIcon('chevron-left', 'sh-chevron-left');
+  api.replaceIcon('chevron-right', 'sh-chevron-right');
+  api.replaceIcon('chevron-up', 'sh-chevron-up');
+  api.replaceIcon('comment-lines', 'sh-comment-lines');
+  api.replaceIcon('exclamation-square', 'sh-exclamation-square');
+  api.replaceIcon('heart', 'sh-heart');
+  api.replaceIcon('link', 'sh-link');
+  api.replaceIcon('lock', 'sh-lock');
+  api.replaceIcon('pencil', 'sh-pencil');
+  api.replaceIcon('search', 'sh-search');
+  api.replaceIcon('shield-alt', 'sh-shield-alt');
+  api.replaceIcon('thumbtack', 'sh-thumbtack');
+  api.replaceIcon('user', 'sh-user');
+});
